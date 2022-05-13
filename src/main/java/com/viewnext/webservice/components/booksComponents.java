20ae84636e769c89df786ef2554071443e60bd0c
Alvaroh12 <6002128@PC29AR4E.insags.com>
2022-05-13 11:04:39 +0200

    Mejora: nueva clase en la que se desarrollan los metodos

---
+++ src/main/java/com/viewnext/webservice/components/booksComponents.java
@@ -0,0 +1,53 @@
+package com.viewnext.webservice.components;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.viewnext.webservice.controller.Book;
+
+@Component("booksComponents")
+public class booksComponents {
+
+	public ArrayList<Book> add (String parameter, int cont, ArrayList <Book> listaLibros){
+		Book book = new Book(parameter, cont+"");
+		listaLibros.add(book);
+		return listaLibros;
+	}
+	
+	public ArrayList<Book> listById(String parameter, ArrayList <Book> listaLibros) {
+		ArrayList<Book> listar = new ArrayList<>();
+		
+		for (int i = 0; i < listaLibros.size(); i++) {
+			if (parameter.equals(listaLibros.get(i).getid())) {
+				listar.add(listaLibros.get(i));
+			}	
+		}
+		return listar;
+	}
+	
+	public ArrayList<Book> delete(String id, ArrayList <Book> listaLibros) {
+		for (int i = 0; i < listaLibros.size(); i++) {
+			if (id.equals(listaLibros.get(i).getid())) {
+				listaLibros.remove(i);
+			} 
+		}
+		return listaLibros;
+	}
+	
+	public ArrayList<Book> update(String id, String parameter, ArrayList <Book> listaLibros) {
+		for (int i = 0; i < listaLibros.size(); i++) {
+			if (id.equals(listaLibros.get(i).getid())) {
+				listaLibros.get(i).setBook(parameter);
+			} else {
+
+			}
+		}
+		return listaLibros;
+	}
+
+	
+	
+}
--- src/main/java/com/viewnext/webservice/controller/controladorLibro.java
+++ src/main/java/com/viewnext/webservice/controller/controladorLibro.java
@@ -3,6 +3,8 @@ package com.viewnext.webservice.controller;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.viewnext.webservice.components.booksComponents;
 import com.viewnext.webservice.controller.Book;
 
 @RestController()
@@ -22,13 +25,13 @@ public class controladorLibro {
 	ArrayList <Book> listaLibros = new ArrayList<Book>();
 	int cont = 0;
 	
+	@Autowired
+	@Qualifier("booksComponents")
+	private booksComponents bookComponent;
+	
 	@PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ArrayList<Book> aniadir (@RequestBody String parameter){
-
-		Book book = new Book(parameter, cont+"");
-		cont++;
-		listaLibros.add(book);
-		return listaLibros;
+		return bookComponent.add(parameter, cont++, listaLibros);
 	}
 	
 	@GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -38,42 +41,18 @@ public class controladorLibro {
 	
 	@GetMapping(value = "/{parameter}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ArrayList<Book> listarId(@PathVariable String parameter){
-		
-		ArrayList<Book> listar = new ArrayList<>();
-		
-		for (int i = 0; i < listaLibros.size(); i++) {
-			if (parameter.equals(listaLibros.get(i).getid())) {
-				listar.add(listaLibros.get(i));
-			}	
-		}
-		return listar;
+		return bookComponent.listById(parameter, listaLibros) ;
 	}
 	
 	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ArrayList<Book> borrar(@PathVariable String id) {
-		
-		for (int i = 0; i < listaLibros.size(); i++) {
-			if (id.equals(listaLibros.get(i).getid())) {
-				listaLibros.remove(i);
-			} 
-		}
-		
-		return listaLibros;
+		return bookComponent.delete(id, listaLibros);
 		}
 	
 	
 	@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ArrayList<Book> modificar(@PathVariable String id, @RequestBody String parameter) {
-		
-		for (int i = 0; i < listaLibros.size(); i++) {
-			if (id.equals(listaLibros.get(i).getid())) {
-				listaLibros.get(i).setBook(parameter);
-			} else {
-
-			}
-		}
-		
-		return listaLibros;
+		return bookComponent.update(id, parameter, listaLibros);
 		}
 }
 
